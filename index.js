0f0dcc04364bbe1dbaeff469a240a43bff3455cc
ahmida saleh <ahmidasaleh.f@yahoo.com>
2023-12-10 12:14:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const bodyparser=require("body-parser");
+const request=require("request");
+
+const app=express();
+app.set("port",(process.env.port || 8000));
+app.use(bodyparser.urlencoded({ extended:false}));
+app.use(bodyparser.json());
+
+app.get("/",function(req,res){
+    res.send("hey ,  i'm just fff dsfsfd")
+}
+
+)
+app.listen(app.get("port"),function(){
+    console.log("server is running and  listen on port "+app.get("port"))
+}
+);
