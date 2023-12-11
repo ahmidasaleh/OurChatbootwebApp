6165d0a29625e1c6c14f6aea6e8c63fe0e6eea69
ahmida saleh <ahmidasaleh.f@yahoo.com>
2023-12-11 17:17:10 +0200

    add greeting

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyparser=require("body-parser");
 const request=require("request");
 
 const app=express();
+//const api_url="https://graph.facebook.com/v18.0/me/messenger_profile?access_token=<PAGE_ACCESS_TOKEN>";
+const page_access_token= "EAAFhwOZB3uaMBO1NzlkGHOHsjZAIZCR9ZAzFMl5K9V6ZBZC44urAcGqzbdCs2M8bydGes5CsqU81FNBjSzhnC7D5ifiXY5mYpc7IIUj0iY1uzcrysZB5F9CzQQQzYER9AZBYAx0VPePau3oNM4g0l7PIBuhiLl2impivEfpAhLBLXHCZCCZB97ZBperwzNsfHUAZBqEC2y5hZBLSZAsGjyftgm";
 app.set("port",(process.env.PORT || 8000));
 app.use(bodyparser.urlencoded({ extended:false}));
 app.use(bodyparser.json());
@@ -22,6 +24,31 @@ app.get("/webhook",function(req,res){
     }
     else
     { res.status(403)}
+});
+
+app.get("/setup",function(req,res){
+var data={
+    "greeting":[
+        {
+          "locale":"default",
+          "text":"Hello!"
+        }
+      ]
+};
+
+request(
+    {
+
+        url:"https://graph.facebook.com/v18.0/me/messenger_profile?access_token="+page_access_token,
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        form:data
+    },
+    function(error,response,body){
+        console.log(response)
+        console.log(body)
+    }
+);
 });
 app.listen(app.get("port"),function(){
     console.log("server is running and  listen on port "+app.get("port"))
