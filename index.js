0da13eb5b8e59509193d384bcceac0b6f7c649e2
ahmida saleh <ahmidasaleh.f@yahoo.com>
2023-12-11 16:25:28 +0200

    add token

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ app.use(bodyparser.urlencoded({ extended:false}));
 app.use(bodyparser.json());
 
 app.get("/",function(req,res){
-    res.send("hey ,  i'm just fff مرحبا")
-}
+    res.send("hey ,  i'm just fff dggsdfsfssfsfd")
+});
 
-)
+app.get("/webhook",function(req,res){
+    //let mode = req.query["hub.mode"];
+    const PAGE_VERIFY_TOKEN="ahmidasaleh"
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+    if (token=== PAGE_VERIFY_TOKEN)
+    {
+        res.status(200).send(challenge);    
+    }
+    else
+    { res.status(403).}
+});
 app.listen(app.get("port"),function(){
     console.log("server is running and  listen on port "+app.get("port"))
 }
