541cdef81b8249a1aed19d9086d52ed5fb162d02
ahmida saleh <ahmidasaleh.f@yahoo.com>
2023-12-12 10:54:48 +0200

    setuppersistent_menu

--- index.js
+++ index.js
@@ -52,6 +52,54 @@ function setupgetstartedbutton()
      
 }
 
+function setuppersistent_menu()
+{
+    var data={
+        {
+            "persistent_menu": [
+                {
+                    "locale": "default",
+                    "composer_input_disabled": false,
+                    "call_to_actions": [
+                        {
+                            "type": "postback",
+                            "title": "Talk to an agent",
+                            "payload": "CARE_HELP"
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Outfit suggestions",
+                            "payload": "CURATION"
+                        },
+                        {
+                            "type": "web_url",
+                            "title": "Shop now",
+                            "url": "https://www.originalcoastclothing.com/",
+                            "webview_height_ratio": "full"
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+    
+    request(
+        {
+    
+            url:"https://graph.facebook.com/v18.0/me/messenger_profile?access_token="+page_access_token,
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            form:data
+        },
+        function(error,response,body){
+            console.log(response);
+            console.log(body);
+        }
+    );
+     
+}
+
+
 function setupegreetingtext(){
     var data={
         "greeting":[
